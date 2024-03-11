1c8a775fd72c7f008f00ec49842714b24aa6567d
purpleracc00n <47504061+purpleracc00n@users.noreply.github.com>
2024-03-11 16:59:01 +0000

    Update __init__.py

--- squarephish/modules/server/__init__.py
+++ squarephish/modules/server/__init__.py
@@ -57,7 +57,7 @@ def init_app(config: ConfigParser, emailer: Emailer) -> redirect:
         # Get user information from the incoming request
         target_email = base64.b64decode(request.args.get("token")).decode('utf-8').strip()
         logging.info(f"{request.headers.get('X-Forwarded-For')} Target [{target_email}] arrived at URL: {request.url}")
-        notify_slack(config.get("SLACK","WEBHOOK"),"Email Opened",target_email,request.headers.get('X-Forwarded-For'),request.headers.get('User-Agent'))
+        notify_slack(config.get("SLACK","WEBHOOK"),"QR Accessed / Clicked Link",target_email,request.headers.get('X-Forwarded-For'),request.headers.get('User-Agent'))
 
         if not target_email:
             logging.error(f"Could not retrieve target email address: '{request.url}' from {request.headers.get('X-Forwarded-For')}")  # fmt: skip
